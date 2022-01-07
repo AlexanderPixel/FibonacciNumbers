98cb58a30162c676c51e191dc5dbc368ecfb7fa1
Alexander <58190800+AlexanderPixel@users.noreply.github.com>
2022-01-07 14:41:40 +0200

    style: formatted tabs and spaces

--- main.cpp
+++ main.cpp
@@ -1,25 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int Fibonacci(int num=0)
+int Fibonacci(int num = 0)
 {
-    if(num<1)
+    if (num < 1)
     {
         return 0;
     }
-    if(num==1)
+    if (num == 1)
     {
         return 1;
     }
-    return Fibonacci(num-1)+Fibonacci(num-2);
+    return Fibonacci(num - 1) + Fibonacci(num - 2);
 }
 
 int main()
 {
-    int position=0;
-    cout<< "Enter the position-> "<<endl;
+    int position = 0;
+    cout << "Enter the position -> " << endl;
     cin >> position;
     
-    cout<<Fibonacci(position)<<endl;
+    cout << Fibonacci(position) << endl;
 }
 
